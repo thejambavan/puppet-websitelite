47f749d148444198fd7385469348ca310382e0cc
Seth Tunstall <seth@tunstall.in>
2024-09-04 17:52:37 +0100

    add extra module config

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,14 +3,19 @@
 # !!! STILL IN DEVELOPMENT !!!
 #
 class websitelite(
-  $http_vhosts      = undef,
-  $https_vhosts     = undef,
-  $certs            = undef,
-  $apache_mods      = undef,
-  $blessed          = true, # "blessed" hosts may issue LE certs
-  $saml_vhosts      = undef,
-  $status           = false,
-  $monitoring       = true,
+  $http_vhosts          = undef,
+  $https_vhosts         = undef,
+  $certs                = undef,
+  $apache_mods          = undef,
+  $blessed              = true, # "blessed" hosts may issue LE certs
+  $saml_vhosts          = undef,
+  $status               = false,
+  $monitoring           = true,
+  $extra_mods           = undef,
+  $oidcclientid         = undef,
+  $oidcclientsecret     = undef,
+  $oidccryptopassphrase = undef,
+
 ) {
   include ::apache
   include ::letsencrypt
@@ -24,6 +29,32 @@ class websitelite(
       include "apache::mod::${module}"
     }
   }
+  if $extra_mods{
+    $extra_mods.each | String $mod_name, Hash $content | {
+
+      concat { "${mod_name}.conf":
+        owner  => 0,
+        group  => 0,
+        path   => "${apache::params::mod_dir}/${mod_name}.conf",
+        mode   => $apache::file_mode,
+        notify =>  Class['Apache::Service'],
+      }
+
+      concat::fragment { "web_mod_00-${mod}":
+        target  => "${apache::params::mod_dir}/${mod_name}.conf",
+        order   => '01',
+        content => inline_template("# Puppet generated Module configuration for ${mod_name}\n<% @content.keys.sort.each do |key| %>  <%= key %> <%= @content[key] %>\n<% end %>"),
+      }
+      file {"${mod_name} symlink":
+        ensure => link,
+        path   => "${apache::params::mod_enable_dir}/${mod_name}.conf",
+        target => "${apache::params::mod_dir}/${mod_name}.conf",
+        mode   => $apache::file_mode,
+        notify => Class['apache::service'],
+      }
+    }
+  }
+
 
   if ( 'security' in $apache_mods ) {
     include ::websitelite::waf
